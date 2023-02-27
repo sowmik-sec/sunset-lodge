d3d126f07d515654ef3bbe88848a29e8afd07458
sowmik-sec <sowmik.sec@gmail.com>
2023-02-27 19:11:25 +0600

    tracking current user

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,7 +1,8 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
@@ -9,13 +10,20 @@ import app from "../firebase/firebase.config";
 const auth = getAuth(app);
 export const AuthContext = createContext();
 const UserContext = ({ children }) => {
+  const [user, setUser] = useState();
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const login = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
-  const authInfo = { createUser, login };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe;
+  }, []);
+  const authInfo = { createUser, login, user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
